07e532f6c0f7d92edc043dae967fa6bdbaf3d92b
JonathanRaiden1780 <mejahv_hs@hotmail.com>
2023-12-28 10:56:07 -0600

    prueba de esBuild

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Finanzeasy',
-  webDir: 'www',
+  webDir: 'www\browser',
   server: {
     androidScheme: 'https'
   }
